1fd7ee623ea4730c07fcbcf4c63691807d869ca2
ajgem <79936721+ajgem@users.noreply.github.com>
2021-09-20 14:10:59 -0400

    Create factor.c

---
+++ factor.c
@@ -0,0 +1,47 @@
+/*Write a program that takes a single integer n as a command-line argumentand finds its prime factors, excluding 1.  
+The output should consist of a single line listing each prime factor in non-decreasing order, separated by a space. 
+If n is prime, just print n itself. If there are repeat factors, print the factor as many times as it divides n. 
+You can assume that2≤n≤231−1(i.e.,n will fit in a signed 32-bit integer).
+
+Take an input as the commane line 
+    //use argv and atoi to get the input and place into function factor 
+creating factor.c 
+    // find the prime factors 
+    // use math.h to find the dividends 
+    // for loop to print out 
+output 
+    // increasing order such as 13, 17 
+    // if n is prime print out n 
+    // if factors are repeated then print them out as many times needed // 32 = 2 2 2 2 2
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+ 
+// prints factors of prime
+void factorCheck(int n)
+{
+    while (n % 2 == 0)
+    {
+        printf("%d ", 2);
+        n = n / 2;
+    }
+ // n is now odd so find the sqrt using math.h
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        // while n is not leaving a reminder print out i 
+        while (n % i == 0)
+        {
+            printf("%d ", i);
+            n = n / i;
+        }
+    }
+}
+ // driver for test cases
+int main()
+{
+    signed int n;
+    scanf("%d", &n);
+    factorCheck(n);
+    return EXIT_SUCCESS;
+}
