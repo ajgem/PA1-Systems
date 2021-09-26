738d31326a56aec3717625cb6144cf936ed6f582
ajgem <79936721+ajgem@users.noreply.github.com>
2021-09-26 13:07:57 -0400

    Create uniq.c
    restarting code

---
+++ uniq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <ctype.h>
+
+//be able to read from stdin 
+main (int argc, char* argv[argc+1]){
+int word;
+  while ( (word = gerchar()) != EOF)
+     putchar(tolower(word);
+             return 0;
+             
