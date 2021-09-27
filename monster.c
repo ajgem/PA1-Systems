ceed0351353ea53c9532ddab336fdf4067949870
asifh063 <62316164+asifh063@users.noreply.github.com>
2021-09-27 19:22:48 -0400

    takes user input to move player pawn
    Problems:
    - orientation not correct
    - previous player pawn does not get removed
    - monster pawn is not moving

--- monster.c
+++ monster.c
@@ -3,16 +3,16 @@ This program is a game of a player and monster moving on a 2D grid. The player h
 In each turn, the player moves one square, then the monster moves towards the player. If either reaches their target, the game ends immediately. If the player reaches the goal, it pr\
 ints “player wins!”. If the monster reaches the player, it prints “monster wins!”.                                                                                                     
 The player’s moves (N, W, S, or E) are read from stdin, one per line. If the move is invalid (e.g., the player tries to move off the board, print “invalid move” and read a new move).\
-If the player intentionally moves into the monster’s square, it counts as the monster winning.                                                                                        
+ If the player intentionally moves into the monster’s square, it counts as the monster winning.                                                                                        
 On its turn, the monster considers the horizontal and vertical distances to reach the player and moves to reduce the larger of these two. If they’re the same, the monster chooses the\
-vertical direction. Its move should be printed, e.g., “monster moves E”.                                                                                                              
+ vertical direction. Its move should be printed, e.g., “monster moves E”.                                                                                                              
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-#define MAX 25
+//#define MAX 25                                                                                                                                                                       
 
 int main(int argc, char* argv[]){
 
@@ -24,12 +24,9 @@ int main(int argc, char* argv[]){
   int goalY = atoi(argv[6]);
   int monX = atoi(argv[7]);
   int monY = atoi(argv[8]);
-  char gameBoard[MAX][MAX];
+  char gameBoard[row][col];                                                                                                                                        
+  //char transpose[MAX][MAX];                                                                                                                                                          
 
-  //scanf("%d %d", &row, &col);                                                                                                                                                        
-  //scanf("%d %d", &playX, &playY);                                                                                                                                                    
-  //scanf("%d %d", &goalX, &goalY);                                                                                                                                                    
-  //scanf("%d %d", &monX, &monY);                                                                                                                                                      
 
   for (int i = 0; i < row; i++){
     for (int j = 0; j < col; j++){
@@ -41,5 +38,39 @@ int main(int argc, char* argv[]){
     }
     printf("\n");
   }
+  char choice;
+
+  scanf("%c ", &choice);
+
+  for (int i = 0; i < row; i++){
+    for (int j = 0; j < col; j++){
+    if (choice == 'N'){
+      gameBoard[playX][playY + 1] = 'P';
+      gameBoard[goalX][goalY] = 'G';
+      gameBoard[monX][monY] = 'M';
+      printf("%c ", gameBoard[i][j]);
+    }
+    else if (choice == 'E'){
+      gameBoard[playX + 1][playY] = 'P';
+      gameBoard[goalX][goalY] = 'G';
+      gameBoard[monX][monY] = 'M';
+      printf("%c ", gameBoard[i][j]);
+    }
+    else if (choice == 'W'){
+      gameBoard[playX - 1][playY] = 'P';
+      gameBoard[goalX][goalY] = 'G';
+      gameBoard[monX][monY] = 'M';
+      printf("%c ", gameBoard[i][j]);
+    }
+    else if (choice == 'S'){
+      gameBoard[playX][playY - 1] = 'P';
+      gameBoard[goalX][goalY] = 'G';
+      gameBoard[monX][monY] = 'M';
+      printf("%c ", gameBoard[i][j]);
+    }
+    }
+    printf("\n");
+    }
+
   return 0;
 }
