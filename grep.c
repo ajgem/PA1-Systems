fce7097846acdc56244985a377586b4eea4172b9
asifh063 <62316164+asifh063@users.noreply.github.com>
2021-09-27 17:32:21 -0400

    compile error fixed

--- grep.c
+++ grep.c
@@ -1,9 +1,10 @@
-/*Write a program that takes a string as a command-line argument and then
-reads from stdin until the user closes stdin (with ctrl-d). It should print
-every line that contains the given string. It should be case-sensitive by
-default, but case-insensitive if the command line option -i is given before
+/*Write a program that takes a string as a command-line argument and then                                                                                                              
+reads from stdin until the user closes stdin (with ctrl-d). It should print                                                                                                            
+every line that contains the given string. It should be case-sensitive by                                                                                                              
+default, but case-insensitive if the command line option -i is given before                                                                                                            
 the string.*/
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,7 +37,7 @@ int main(int argc, char* argv[]) {
          ret = strstr(input, grepWord);
       }
       else{
-         ret = strcasestr(input, grepWord);
+        ret = strcasestr(input, grepWord);
       }
 
       if(ret){
@@ -45,3 +46,4 @@ int main(int argc, char* argv[]) {
    };
    return 0;
 }
+
